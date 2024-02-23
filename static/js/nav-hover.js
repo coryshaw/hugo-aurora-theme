ab94597e6ddf923a1daafe2a2e69f5a37e6e892b
Cory Shaw <coryshaw@gmail.com>
2024-02-22 20:09:05 -0600

    hover script updates

--- static/js/nav-hover.js
+++ static/js/nav-hover.js
@@ -4,16 +4,38 @@ document.addEventListener("DOMContentLoaded", () => {
   var nav = document.querySelector("nav"); // Select the <nav> element
   const parentBounds = nav.parentElement.getBoundingClientRect(); // Get the bounds of the parent element
   nav.appendChild(hoverBorder);
-  const padding = 5; // Padding around the hovered element
-  document.querySelectorAll(".primary-nav a").forEach((item) => {
+  const padding = 0; // Padding around the hovered element
+
+  const setHoverBorderStyle = (element) => {
+    const bounds = element.getBoundingClientRect();
+    hoverBorder.style.width = `${bounds.width + padding * 2}px`; // Add padding to width
+    hoverBorder.style.height = `${bounds.height + padding * 2}px`; // Add padding to height
+    hoverBorder.style.top = `${bounds.top + window.scrollY - padding}px`; // Subtract padding from top
+    hoverBorder.style.left = `${
+      bounds.left + window.scrollX - parentBounds.left - padding
+    }px`; // Subtract padding from left
+  };
+
+  const primaryNavLinks = document.querySelectorAll(".primary-nav a");
+  primaryNavLinks.forEach((item) => {
     item.addEventListener("mouseenter", (e) => {
-      const bounds = e.target.getBoundingClientRect();
-      hoverBorder.style.width = `${bounds.width + padding * 2}px`; // Add padding to width
-      hoverBorder.style.height = `${bounds.height + padding * 2}px`; // Add padding to height
-      hoverBorder.style.top = `${bounds.top + window.scrollY - padding}px`; // Subtract padding from top
-      hoverBorder.style.left = `${
-        bounds.left + window.scrollX - parentBounds.left - padding
-      }px`; // Subtract padding from left
+      setHoverBorderStyle(e.target);
+    });
+    item.addEventListener("mouseleave", () => {
+      const selectedElement = document.querySelector(
+        ".primary-nav li.selected a"
+      );
+      if (selectedElement) {
+        setHoverBorderStyle(selectedElement);
+      }
     });
   });
+
+  // Check if any <li> element has a "selected" class
+  const selectedElement = document.querySelector(".primary-nav li.selected a");
+  if (selectedElement) {
+    setTimeout(() => {
+      setHoverBorderStyle(selectedElement);
+    }, 1000); // 3000 milliseconds = 3 seconds
+  }
 });
